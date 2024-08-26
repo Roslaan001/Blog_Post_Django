1d162ba15d470885429cbd3f353f5cf5b0cf188b
Roslaan001 <abdulsomad005@gmail.com>
2024-08-26 07:36:26 +0100

    env

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,10 +31,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)j0^@m33339p0odan@)t!!k65*33v61g6dq9-cxua^pyob^f4z'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
