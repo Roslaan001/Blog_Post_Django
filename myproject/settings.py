30cf34cf5cb9a27e3bf25b7c0f9c9974ca83dc66
Roslaan001 <abdulsomad005@gmail.com>
2024-09-07 17:59:17 +0100

    add gunicorn

--- myproject/settings.py
+++ myproject/settings.py
@@ -107,10 +107,6 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL")), 
-# }
-
 
 
 DATABASES = {
