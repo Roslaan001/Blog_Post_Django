92d788288547ca0c51530ae31d316c846a31f005
Roslaan001 <abdulsomad005@gmail.com>
2024-09-08 16:48:44 +0100

    Whitenoise

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,9 +37,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = os.environ.get('DEBUG') DEBUG = 'RENDER' not in os.environ
+
 
 ALLOWED_HOSTS = ['*']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Email Authentication
 
